f68da8e85646bc7721f010f8a19e9b41bf97174f
Ngoc Minh <hangoc.minh1306@gmail.com>
2022-12-27 21:10:01 +0700

    update animation and layout
    Add several things

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
+// Fly-in animation
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//     }
+//   });
+// });
+// const navCards = document.querySelectorAll('.nav-card');
+// navCards.forEach((card) => {
+//   observer.observe(card);
+// });
+// observer.observe(navCards);
+// // Work Cards logic
+// const worksCard = document.querySelector('.works-card');
+// const shapes = document.querySelectorAll('.works-card > .shape');
+// const worksAnimation = {
+//   top: '50%',
+//   left: '50%',
+//   width: '40%',
+//   height: '30%',
+//   transform: 'translate(-50%, -50%)',
+//   borderRadius: '5px',
+// };
+// worksCard.onmouseover = () => {
+//   shapes.forEach((shape) => {
+//     shape.animate(worksAnimation, {
+//       duration: 750,
+//       fill: 'forwards',
+//       easing: 'ease-in-out',
+//     });
+//   });
+// };
+// worksCard.onmouseout = () => {
+//   shapes.forEach((shape) => {
+//     shape.animate(worksAnimation, {
+//       duration: 750,
+//       direction: 'alternate',
+//       easing: 'ease-in-out',
+//     });
+//   });
+// };
+// Skill cards logic
 const eye = document.querySelector('.eye');
 const eyeBall = document.querySelector('.eye-ball');
 const skillsCard = document.querySelector('.skills-card');
-
 // Eyes follow cursor logic
 skillsCard.onmousemove = (e) => {
   const rect = skillsCard.getBoundingClientRect();
@@ -36,7 +78,7 @@ skillsCard.onmousemove = (e) => {
   );
 };
 // Eye goes back to  original position
-skillsCard.onmouseout = (e) => {
+skillsCard.onmouseout = () => {
   eyeBall.animate(
     {
       transform: `translate(-50%, -50%)`,
